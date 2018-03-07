8cb2effbe4324664c0927db03ec6643f74fabb03
Thimóteo <Thimteo@10.50.70.210>
2018-03-07 15:05:50 -0400

    Criado metodo subtracao

--- ProjetoGitJava/src/br/edu/ifro/Calculadora.java
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -9,6 +9,9 @@ public class Calculadora {
         return num1 + num2;
     }
 
+    public double subtracao(double num1, double num2) {
+        return num1 - num2;
+    }
     // TODO subtracao
     // TODO divisao
     // TODO multiplicação    

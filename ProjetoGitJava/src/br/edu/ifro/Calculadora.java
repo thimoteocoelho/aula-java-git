831ef39d09b3a18ee72d4b62d4e95bd8bdb3565b
Diego Fonseca <dfonseca404@gmail.com>
2018-03-07 14:53:22 -0400

    Criando a classe calculadora

---
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -0,0 +1,15 @@
+package br.edu.ifro;
+
+/**
+ * @author diego da fonseca
+ */
+public class Calculadora {
+
+    public double soma(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    // TODO subtracao
+    // TODO divisao
+    // TODO multiplicação    
+}
